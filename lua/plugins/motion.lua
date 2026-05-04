c9073a59af7c860ca7360a727595f169500bb988
duhan <duhan@189.cn>
2026-05-04 11:34:45 +0800

    .

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -5,78 +5,16 @@ return {
     ---@type Flash.Config
     opts = {
       modes = {
-        -- 让 search 模式也使用 flash（/ 和 ? 之后的高亮/跳转更好用）
+        -- 仅启用 search 模式的 flash（/ 和 ? 之后的高亮/跳转更好用）
         search = {
           enabled = true,
         },
+        -- 其余模式保持默认行为（不接管按键）
+        char = { enabled = false },
+        treesitter = { enabled = false },
       },
     },
-    keys = {
-      -- 常用：类似 "s" 快速跳到屏幕内任意位置
-      {
-        "s",
-        function()
-          require("flash").jump()
-        end,
-        mode = { "n", "x", "o" },
-        desc = "Flash: jump",
-      },
-      -- 选中 Treesitter 节点/语法单元
-      {
-        "S",
-        function()
-          require("flash").treesitter()
-        end,
-        mode = { "n", "x", "o" },
-        desc = "Flash: treesitter",
-      },
-      -- 远程操作（类似 easymotion 的远程编辑/选择能力）
-      {
-        "r",
-        function()
-          require("flash").remote()
-        end,
-        mode = "o",
-        desc = "Flash: remote",
-      },
-      {
-        "R",
-        function()
-          require("flash").treesitter_search()
-        end,
-        mode = { "o", "x" },
-        desc = "Flash: treesitter search",
-      },
-
-      -- 保持你原先的 leader 习惯：
-      -- <leader>f：跨窗口 jump（接近 easymotion-overwin-w）
-      {
-        "<leader>f",
-        function()
-          require("flash").jump({
-            search = {
-              multi_window = true,
-            },
-          })
-        end,
-        mode = { "n", "x", "o" },
-        desc = "Flash: jump (multi-window)",
-      },
-      -- <leader>j：向下跳（这里做成 "向下" 的字符落点）
-      {
-        "<leader>j",
-        function()
-          require("flash").jump({
-            search = {
-              forward = true,
-              wrap = false,
-              multi_window = true,
-            },
-          })
-        end,
-        mode = { "n", "x", "o" },
-        desc = "Flash: jump forward",
-      },
-    },
+    -- 去掉所有自定义 keys，让 s/S/r/R/<leader>f/<leader>j 等保持 nvim 默认行为
+    keys = {},
   },
 }
