3893f9666bb77a7d313312407b5266ad3cf5cb39
duhan <duhan@189.cn>
2026-06-23 14:39:42 +0800

    .

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,13 +1,33 @@
 return {
   {
     "nvim-orgmode/orgmode",
+
+    -- 仍然按 org 文件懒加载，但也允许通过命令触发加载（否则你在非 org buffer 里用不了 agenda）
     ft = { "org" },
+    cmd = { "OrgAgenda", "OrgAgendaDay", "OrgAgendaWeek" },
+
     config = function()
       require("orgmode").setup({
         org_agenda_files = { "~/org/**/*" },
         org_default_notes_file = "~/org/notes.org",
         win_split_mode = "vertical",
+        org_agenda_span = "day", -- 默认只看今天
       })
+
+      -- 自定义命令：日/周视图（临时切换）
+      vim.api.nvim_create_user_command("OrgAgendaDay", function()
+        vim.cmd("OrgAgenda day")
+      end, {})
+
+      vim.api.nvim_create_user_command("OrgAgendaWeek", function()
+        vim.cmd("OrgAgenda week")
+      end, {})
+
+      -- 快捷键（按你习惯可改）
+      -- <leader>od: 打开 day agenda
+      -- <leader>ow: 打开 week agenda
+      vim.keymap.set("n", "<leader>od", "<cmd>OrgAgendaDay<cr>", { desc = "Org agenda: day" })
+      vim.keymap.set("n", "<leader>ow", "<cmd>OrgAgendaWeek<cr>", { desc = "Org agenda: week" })
 
       -- 修复/增强 org 中 TODO / DONE 等关键字高亮（避免在 molokai 下不清晰）
       -- 同时增强 agenda 里的 scheduled 标记（你 Inspect 看到的是 @org.agenda.scheduled）
